1955e950f43f1b1e844d0dc7d60d54e5a19c68bc
uwumukiza123 <uwumukizalaetitia@gmail.com.com>
2022-10-18 13:16:16 -0700

    task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+
 /**
- * _putchar - our function
- * @c: character
- * Return: always character
- *
+ * main - print _putchar
+ * is written by Laetitia
+ *  Return: 0
  */
-int _putchar(char c);
-#endif
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
