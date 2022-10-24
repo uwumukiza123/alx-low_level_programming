85952a37fdf711f5b16d736ab7cbfc71bfac8455
uwumukiza123 <uwumukizalaetitia@gmail.com.com>
2022-10-24 15:16:25 -0700

    task-9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,17 +3,19 @@
 
 /**
  * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
+ * @dest: destination of the string
+ * @src: source of the string
  * Return: string
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	char *guard = dest;
 
-	for (; src[i] != '\0'; i++)
+	while (*src)
 	{
-		dest[i] = src[i];
+		*dest++ = *src++;
+		*dest = 0;
 	}
-	return (dest);
+	return (guard);
 }
