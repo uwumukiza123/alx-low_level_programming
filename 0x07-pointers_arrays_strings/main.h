aaa1e460a51ba037e0d763935ded3c0ed14c90d3
uwumukiza123 <uwumukizalaetitia@gmail.com.com>
2022-10-31 15:19:03 -0700

    task-0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN.H
 
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -14,4 +14,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif
