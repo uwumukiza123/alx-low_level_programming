ad9493f0d7317210c7a254bf519d3c719549aed6
uwumukiza123 <uwumukizalaetitia@gmail.com.com>
2022-11-02 12:33:15 -0700

    task-4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
